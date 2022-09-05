d19f3ece7347ebcecb6e1da455174ad9d183fd48
Charles <charlesarchy09@gmail.com>
2022-09-04 21:51:38 -0500

    initial commit states autocomplete

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyp= require("body-parser")
+const ejs = require("ejs");
+const path = require("path")
+
+//setting the ejs envornment
+const app = express();
+
+
+ //home page
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+//get in touch with the server
+app.set('view engine', 'ejs');
+app.use(bodyp.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3002, ()=>{
+  console.log("page is running on port 3002")
+});
